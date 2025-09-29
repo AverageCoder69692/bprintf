273d5657dbbbb561e8c7fb4fe0cecd6d8c0e3ab2
AverageCoder69692 <c61198108@gmail.com>
2025-09-29 20:15:06 +0530

    Update bprintf.C

--- bprintf.C
+++ bprintf.C
@@ -3,6 +3,8 @@ typedef __builtin_va_list va_list;
 #define va_arg(ap, type) __builtin_va_arg(ap, type)
 #define va_end(ap) __builtin_va_end(ap)
 
+typedef unsigned int uintptr_t;
+
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((unsigned short*)0xB8000)
