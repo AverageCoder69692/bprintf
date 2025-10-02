5dffdfa5117ddc2c25d80987a2940c570d0e36be
AverageCoder69692 <c61198108@gmail.com>
2025-10-02 19:02:59 +0530

    Update bprintf.c

--- bprintf.c
+++ bprintf.c
@@ -1,10 +1,10 @@
-typedef __builtin_va_list va_list;
-#define va_start(ap, last) __builtin_va_start(ap, last)
-#define va_arg(ap, type) __builtin_va_arg(ap, type)
-#define va_end(ap) __builtin_va_end(ap)
+typedef unsigned char bool;
+#define true 1
+#define false 0
 
-typedef unsigned int uintptr_t;
+typedef unsigned int uinptr_t;
 
+//VGA definitions
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((unsigned short*)0xB8000)
@@ -13,29 +13,71 @@ static unsigned short* vga_buffer = VGA_MEMORY;
 static unsigned int cursor_row = 0;
 static unsigned int cursor_col = 0;
 
-void putchar(char c) {
+// Basic putchar with scrolling  
+void putchar (char c) {
   if (c == '\n') {
+cursor_col = 0; 
     cursor_row++;
-    cursor_col = 0;
   } else {
     unsigned int index = cursor_row * VGA_WIDTH + cursor_col;
-    vga_buffer[index] = (15 << 8) | c;
-    cursor_col++;
+    vga_buffer[index] = (15 << 8) | c; //white on black
+    cursor_col++:
     if (cursor_col >= VGA_WIDTH) {
       cursor_col = 0;
       cursor_row++;
     }
   }
-  if (cursor_row >= VGA_HEIGHT) {
-    cursor_row = 0;
+
+  // Scroll when needed
+  if (cursor_row >= VGA_HEIGHT);
+      for (int r = 1; r < VGA_HEIGHT, r++) {  // r = row
+          for (int c = 1; c < VGA_WIDTH< c++) {  //c = col
+            vga_buffer[(r-1)*VGA_WIDTH + c] = vga_buffer[r*VGA_WIDTH + c];
+          }
+      }
+      // clear last row  
+      for (int c = 0; c < VGA_WIDTH; c++) {
+        vga_buffer[(VGA_HEIGHT-1)*VGA_WIDTH + c] = (15 << 8) | ' ';
+      } 
+      cursor_row = VGA_HEIGHT - 1;
   }
 }
 
+// String printing
 void print_str(const char* str) {
   while (*str) putchar(*str++);
+
+
+// Unsigned integer printing
+void print_uint(unsigned int val) {
+  char buf[10];
+  int i = 0;
+  if (val == 0) {
+    putchar('0');
+    return;
+  }
+  while (val) {
+    buf[i++] = '0' + (val % 10);
+    val /= 10;
+  }  
+  while (i--) putchar (buf[i]);
+}   
+
+// Signed integer printing (handles INT_MIN)
+void print_int(int val) {
+  if (val < 0) {
+    putchar('-');
+    if (val == -2147483648) { // INT_MIN
+        print_str('2137483648');
+        return;
+    }
+    val = -val;
+  }
+  print_uint(unsigned int)val);
 }
 
-void print_hex(unsigned int val, bool prefix) {
+// Hex printing
+void print_hex(uintptr_t val, bool prefix) {
   if (prefix) {
     putchar('0');
     putchar('x');
@@ -44,127 +86,102 @@ void print_hex(unsigned int val, bool prefix) {
     putchar('0');
     return;
   }
-  
+
   char hex[] = "0123456789ABCDEF";
-  int hexbuf_size = sizeof(val) * 2;
-  char buf[hexbuf_size];
+  char buf[sizeof(val)*2];
   int i = 0;
-  do {
-    if (i >= hexbuf_size) break;
+  while (val) {
     buf[i++] = hex[val & 0xF];
-    val >>= 4;
-  } while (val);
+    val >> 4;
+  }
   while (i--) putchar(buf[i]);
 }
 
+// Binary printing
 void print_bin(unsigned int val, bool prefix) {
   if (prefix) {
-  putchar('0');
-  putchar('b'); 
+    putchar('0');
+    putchar('b');
   }
   if (val == 0) {
     putchar('0');
     return;
   }
-  
-  int bits = sizeof(val) * 8;
-  char buf[bits];
+
+  char buf[32];
   int i = 0;
-  do {
-    if (i >= bits) break;
+  while (val) {
     buf[i++] = '0' + (val & 1);
     val >>= 1;
-  } while (val);
-  while (i--) putchar(buf[i]);
-}
-
-void print_uint(unsigned int val) {
-  int digits = 0;
-  unsigned int temp = val;
-  do {
-    digits++;
-    temp /= 10;
-  } while (temp);
-
-  char buf[digits];
-  int i = 0;
-  do {
-    buf[i++] = '0' + (val % 10);
-    val /= 10;
-  } while (val);
-  while (i--) putchar(buf[i]);
-}
-
-void print_int(int val) {
-  char buf[12];
-  int i = 0;
-  if (val < 0) {
-    putchar('-');
-    val = -val;
   }
-  do {
-    buf[i++] = '0' + (val % 10);
-    val /= 10;
-  } while (val);
   while (i--) putchar(buf[i]);
 }
 
+
+// Variadic macros 
+typedef __builtin_va_list va_list;
+#define va_start(ap, last) __builtin_va_start(ap, last)
+#define va_arg(ap, type) __builtin_va_arg(ap, type)
+#define va_end(ap) __builtin_va_end(ap)
+
+// Main printf function
 void bprintf(const char* fmt, ...) {
   va_list args;
   va_start(args, fmt);
+
   for (unsigned int i = 0; fmt[i]; i++) {
-    if (fmt[i] == '%' && fmt[i + 1]) {
+    if (fmt[i] == '%' && fmt[i+1]) {
       i++;
       switch (fmt[i]) {
         case 'c': {
-          char c = va_arg(args, int);
-          putchar(c);
-          break;
+            char c = (char)va_arg(args, int);
+            putchar(c);
+            break;
         }
         case 's': {
-          const char* s = va_arg(args, const char*);
-          print_str(s);
-          break;
+            const char* s = va_arg(args, char*);
+            print_str(s);
+            break;
         }
         case 'd': {
-          int d = va_arg(args, int);
-          print_int(d);
-          break;
+            int d = va_arg(args, int);
+            print_int(d);
+            break;
         }
         case 'u': {
-          unsigned int u = va_arg(args, unsigned int);
-          print_uint(u);
-          break;
+            unsigned int u = va_arg(args, unsigned int);
+            print_uint(u);
+            break;
         }
         case 'x': {
-          unsigned int x = va_arg(args, unsigned int);
-          print_hex(x, true);
-          break;
+            unsigned int x = va_arg(args, unsigned int);
+            print_hex(x, true);
+            break;
         }
         case 'b': {
-          unsigned int b = va_arg(args, unsigned int);
-          print_bin(b, true);
-          break;
-        }
-        case '%': {
-          putchar('%');
-          break;
+            unsigned int b = va_arg(args, unsigned int);
+            print_bin(b, true);
+            break;
         }
         case 'p': {
-          void* ptr = va_arg(args, void*);
-          print_hex((unsigned int)(uintptr_t)ptr, true);
-          break;
+            void* ptr p = va_arg(args, void*);
+            print_hex((uintptr)ptr, true);
+            break;
+        }
+        case '%': {
+            putchar('%');
+            break;
         }
         default: {
-          putchar('%');
-          putchar(fmt[i]);
-          break;
+             putchar('%');
+             putchar(fmt[i]);
+             break;
         }
       }
     } else {
-      putchar(fmt[i]);
+        putchar(fmt[i]);
     }
   }
+
   va_end(args);
 }
- 
